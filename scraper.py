04d7ad00c74c86c9ef7d36955bd4aa2304fdbe7b
Josiah Mbao <79065770+josiah-mbao@users.noreply.github.com>
2023-12-17 17:04:56 +0300

    Iterates over the years now
    Added an outer loop to now get data on all days, for all months in ALL years (I think 20 years... depending on how many years needed)

--- scraper.py
+++ scraper.py
@@ -49,11 +49,18 @@ def scrape_and_download(url, download_directory):
                 print(nc_file[variable][:])  # Print the data for the variable (adjust as needed)
 
 # URL pattern for the pages set to year 2000 January.
-base_url = "https://noaa-cdr-precip-cmorph-pds.s3.amazonaws.com/index.html#data/hourly/0.25deg/2000/01/"
+base_url_template = "https://noaa-cdr-precip-cmorph-pds.s3.amazonaws.com/index.html#data/hourly/0.25deg/{year:04d}/{month:02d}/{day:02d}/"
 download_base_directory = "downloaded_files"
 
-# Iterate over the range of pages
-for day in range(1, 32):  # assuming days from 1 to 31
-    url = f"{base_url}{day:02d}/"
-    download_directory = os.path.join(download_base_directory, f"day_{day:02d}")
-    scrape_and_download(url, download_directory)
+# Iterate over the range of years
+for year in range(2000, 2021):  # assuming years from 2000 to 2020
+    # Iterate over the range of months (1 to 12)
+    for month in range(1, 13):
+        # This checks how many days are in a given month (adjust as needed)
+        days_in_month = 31 if month in [1, 3, 5, 7, 8, 10, 12] else 30 if month in [4, 6, 9, 11] else 29 if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) else 28
+        
+        # Iterates over the range of days for the current month
+        for day in range(1, days_in_month + 1):
+            base_url = base_url_template.format(year=year, month=month, day=day)
+            download_directory = os.path.join(download_base_directory, f"year_{year:04d}_month_{month:02d}_day_{day:02d}")
+            scrape_and_download(base_url, download_directory)
